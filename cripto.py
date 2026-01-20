e5e805e7e60f8d3d99debf69ebbcda8d8d2bdb2f
dolarfuturo <clubesdosul@gmail.com>
2026-01-19 22:54:48 -0300

    Update cripto.py

--- cripto.py
+++ cripto.py
@@ -1,13 +1,4 @@
 import streamlit as st
-import requests
-
-# Isso vai extrair o IP real do servidor para você
-ip_alvo = requests.get('https://api.ipify.org').text
-
-st.title("✅ IP Identificado")
-st.write("Copie o número abaixo e cole na Binance:")
-st.code(ip_alvo)
-import streamlit as st
 import ccxt
 import pandas as pd
 
