2f2e75f2880bff13c7fb5a07d3372eb61cae98aa
dolarfuturo <clubesdosul@gmail.com>
2026-01-19 22:29:42 -0300

    Update cripto.py

--- cripto.py
+++ cripto.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import requests
 
-st.title("Configurador de Eixo Real")
-try:
-    ip = requests.get('https://api.ipify.org').text
-    st.success(f"O IP do seu servidor é: {ip}")
-    st.info("Copie o número acima e cole na Binance no campo 'IPs Confiáveis'.")
-except:
-    st.error("Erro ao identificar IP. Verifique sua conexão.")
+# Isso vai extrair o IP real do servidor para você
+ip_alvo = requests.get('https://api.ipify.org').text
+
+st.title("✅ IP Identificado")
+st.write("Copie o número abaixo e cole na Binance:")
+st.code(ip_alvo)
